98941752ae645070ae35fb2247170eb55aaf6cbf
Martijn vtleengoed <martijn@databrein.nl>
2026-02-02 18:20:51 +0100

    deleted

--- models/marts/fct_daily_sales.sql
+++
@@ -1,8 +0,0 @@
-select
-  order_date,
-  country_code,
-  count(*) as orders,
-  sum(totaal_bedrag) as omzet_perdag,
-  sum(net_bedrag) as omzet_net_perdag
-from {{ ref('fct_orders') }}
-group by 1,2
